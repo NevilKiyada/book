34eeb3bca7622323fede8dc8261a37af57001a39
NevilKiyada <nevilkiyada04@gmail.com>
2024-04-20 20:48:20 +0530

    a2

--- book/models.py
+++ book/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class book(models.Model):
     book_title = models.CharField(max_length=200)
-    book_discussion = models.TextField()
+    book_description = models.TextField()
     book_image=models.ImageField(upload_to="book_images")
--- book/views.py
+++ book/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def book (request):
+    if request.method == 'POST':
+        data=request.POST
+        book_title=data.get('book_title')
+        book_description=data.get('book_description')
+        print ("book description=",book_description)
+
+        print ("book Name",book_title)
+
+        
     context={'page':'contect'}
     return render(request,'insert_data.html',context)
