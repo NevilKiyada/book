fcc8c4dc1f221d113256c1c19abb6cde1e634c6e
NevilKiyada <nevilkiyada04@gmail.com>
2024-04-20 19:31:36 +0530

    a1

--- book/views.py
+++ book/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def book (request):
-    return render(request,'insert_data.html')
+    context={'page':'contect'}
+    return render(request,'insert_data.html',context)
